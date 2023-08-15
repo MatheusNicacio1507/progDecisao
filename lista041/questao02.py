876280b0e2e1daf559948029cd3a989b593ba10a
matheus Nicacio <matheussantosnicacio@gmail.com>
2023-08-15 08:53:01 -0300

    questão 2

---
+++ lista041/questao02.py
@@ -0,0 +1,11 @@
+'''
+Desenvolver um programa que permita ao aluno responder qual a capital do Brasil. O programa deverá exibir se
+a resposta está certa ou errada.
+'''
+
+capital = input("Qual é a capital do Brasil? ")
+
+if(capital == "Brasília"):
+    print("Resposta CORRETA!")
+else:
+    print("Resposta ERRADA!")
