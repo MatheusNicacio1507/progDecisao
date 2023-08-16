9a12eda3c264a8cb948314f39d2b281953388cab
matheus Nicacio <matheussantosnicacio@gmail.com>
2023-08-16 10:18:17 -0300

    questão 01 lista 042

---
+++ lista042/questao01.py
@@ -0,0 +1,12 @@
+'''
+Fazer um algoritmo que peça um número, e ao final, informe se o número digitado está acima de 1000 ou
+abaixo de 1000.
+'''
+
+num = int(input("Insira um número: "))
+
+if(num > 1000):
+    print("O número está acima de 1000")
+
+else:
+    print("O número está abaixo de 1000")
